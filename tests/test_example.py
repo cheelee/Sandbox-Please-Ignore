ee8f9f5ec7975f0bcf52df1ebb73592c549baf10
Chee Wai Lee <cheewai1972@gmail.com>
2016-03-03 16:45:55 +0800

    I had not intended to test against python 3.5 quite so soon. Removed this from testing against Travis-CI. Also the example may have been a little too large for the Travis-CI server ... it stalled out after 10 minutes. Trying a much smaller example that takes less than a second on my local desktop, so if it still takes 10 minutes on Travis, something is wrong.

--- tests/test_example.py
+++ tests/test_example.py
@@ -2,8 +2,8 @@ import numpy
 import numpy.matlib
 
 def test_matmult():
-    mat_a = numpy.matlib.rand(10000,5000)
-    mat_b = numpy.matlib.rand(5000,10000)
+    mat_a = numpy.matlib.rand(2000,1000)
+    mat_b = numpy.matlib.rand(1000,5000)
 
     result = numpy.dot(mat_a, mat_b)
-    assert result.size == 100000000
+    assert result.size == 10000000
