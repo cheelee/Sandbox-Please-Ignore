6fec848a484b202e1f0d8fdb185271b4f331a9b3
Chee Wai Lee <cheewai1972@gmail.com>
2016-03-03 16:20:51 +0800

    Changed the basic tests to be non-trivial time-wise. This is to set up the repository for handling subsequent profiling runs.

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,9 +1,9 @@
 import numpy
+import numpy.matlib
 
-def test_numpy():
-    a = numpy.arange(15).reshape(3, 5)
-    assert a.size == 15
+def test_matmult():
+    mat_a = numpy.matlib.rand(10000,5000)
+    mat_b = numpy.matlib.rand(5000,10000)
 
-def test_array():
-    a = numpy.arange(15).reshape(3, 5)
-    assert a[1,1] == 6
+    result = numpy.dot(mat_a, mat_b)
+    assert result.size == 100000000
